b603ecaf23dcfaa1aaae3284d442dc7015fecb50
Marcel Walk <walk.marcel.97@googlemail.com>
2021-01-30 08:53:59 +0100

    Add project files.

---
+++ NewsWall/Models/FeedViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace NewsWall.Models
+{
+    public class FeedViewModel
+    {
+        public IEnumerable<SyndicationItem> Feeds { get; set; }
+
+        public FeedViewModel(string v)
+        {
+            Feeds = GetFeeds(v);
+        }
+
+        public IEnumerable<SyndicationItem> GetFeeds(string url)
+        {
+            XmlReader reader = XmlReader.Create(url);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            reader.Close();
+            return feed.Items;
+        }
+    }
+}
